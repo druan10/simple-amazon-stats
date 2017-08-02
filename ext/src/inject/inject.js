81fcedbdc838bc29d21dda581d59a64afddaab7e
druan10 <druan10@gmail.com>
2017-08-02 16:17:49 -0700

    Generalized Table Building Functions & Added Function to check if id exists

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -6,23 +6,106 @@ chrome.extension.sendMessage({}, function(response) {
 
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		var simpleStatsDiv = document.getElementById("detail-ilm_div");
+
+		console.log("Amazon Data Simplifier Loaded!");
+
+		var targetDivName = "detail-ilm_div2";
+
+		// Div to be replaced with stats table
+		if (!idExists(targetDivName)) {
+			console.log("Shit don't exist fam");
+		}
+
+		var targetDiv = document.getElementById(targetDivName);
+
+		
+		// Default Values
 		var shippingWeight = 0.01;
 		var shippingWeightSource = "product-details";
+		var numOfStatItems = 0;
+		var currentCol = "";
+		var numOfStatRows = 0;
 
-		simpleStatsDiv.innerHTML = '<table style="width:80%;margin-left:auto;margin-right:auto;padding-top:5px;padding-bottom:5px" id="simpleStatsTable"></table>';
-		
-		var simpleStatsTable = getElementById("simpleStatsTable");
+		targetDiv.innerHTML = `
+			<table id = "simpleStatsTable"> 
+				<h3>Simplified Amazon Stats</h3>
+				<form id="weightConverter">
+					Weight in OZ: <input type="number" id="ouncesInput" placeholder="oz"></input> Weight in Pounds: <input type="number" id="poundsOutput" placeholder="lbs" readonly></input>
+				</form>
+			</table> 
+			<hr>`;
 
-		simpleStatsTable.innerHTML += '<tr><td><h3> isAsinMerge: No </h3></td><td><h3> isRefrigerated: No </h3></td></tr>';
+		// Allows real time updates to the weight converter
+		document.getElementById("ouncesInput").addEventListener("keyup", convertOuncesToPounds);
 
-		simpleStatsTable.innerHTML += '<tr><td><h3> Estimated Weight: __ </h3></td><td><h3> <form> <input type="text" name="oz" id="oz_input" value="'+shippingWeight+'"> </form> </h3></td></tr>';
-		// myNode.innerHTML = '<h3> isAsinMerge: No </h3>'
-		// myNode.innerHTML = '<h3> Estimated weight: ' + shippingWeight + ' pounds </h3> (Scraped from ' + shippingWeightSource + '.)';
-		// myNode.innerHTML += '<h3> Convert weight to pounds: <form> <input type="text" name="oz" id="oz_input" value="'+shippingWeight+'"> </form> <h3>'
-		// myNode.innerHTML += "more features and stuff...";
-		// // ----------------------------------------------------------
+		var simpleStatsTable = document.getElementById("simpleStatsTable");
+
+		addStatItem("<b>HEY</b>");
+
+		//TODO, create get weight function
+		// getWeightInPounds();
+
+
+
+
+
+		// ----------------------------------------------------------
+		// Functions for scraping and presenting data
+
+		function addStatItem(htmlToAdd) {
+
+			// If we have an even number of items, then we have to add a new empty row and append item to first col, if not, add to second column
+			if (numOfStatItems%2==0) {
+				addEmtpyRowToStatsTable();
+				currentCol = document.getElementById("row_" + (numOfStatRows) + "_LeftCol");
+				currentCol.innerHTML = htmlToAdd;
+			} else {
+				currentCol = document.getElementById("row_" + (numOfStatRows) + "_RightCol"); 
+				currentCol.innerHTML = htmlToAdd;
+			}
+			numOfStatItems++;
+			console.log(numOfStatItems);
+		}
+
+		function addEmtpyRowToStatsTable() {
+			numOfStatRows++;
+			simpleStatsTable.innerHTML += `
+					<tr id="tableRow_` + (numOfStatRows) + `">
+						<td id="row_` + (numOfStatRows) + `_LeftCol" style="width:50%;"></td>
+						<td id="row_` + (numOfStatRows) + `_RightCol" style="width:50%;"></td>
+					</tr>
+					`;
+		}
+
+		function getWeightInPounds() {
+			var relevantDiv = "";
+			var dataArray = [];
+			relevantDiv = document.getElementById("detail-bullets");
+			console.log(relevantDiv);
+		}
+
+		function convertOuncesToPounds () {
+			console.log("converting");
+			var ounces = document.getElementById("ouncesInput").value;
+			if (!isNaN(ounces)) {
+				document.getElementById("poundsOutput").value = roundTo(ounces / 16, 2);
+			} 
+		}
+
+		// Written by https://stackoverflow.com/users/1634137/shura
+		function roundTo(n, digits) {
+		    if (digits === undefined) {
+		        digits = 0;
+		    }
+
+		    var multiplicator = Math.pow(10, digits);
+		    n = parseFloat((n * multiplicator).toFixed(11));
+		    return Math.round(n) / multiplicator;
+		}
+
+		function idExists(id) {
+			return (document.getElementById(id) != null);
+		}
 
 	}
 	}, 10);
